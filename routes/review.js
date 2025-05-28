4fd1ff45e6362a6feb1d622a3c6365b65424af2c
Divyansh Garg <divyanshgarg844@gmail.com>
2025-05-28 08:59:16 +0530

    Review Authentication

--- routes/review.js
+++ routes/review.js
@@ -5,7 +5,7 @@ const ExpressError = require("../utils/ExpressError.js");
 
 const Review = require("../models/review.js");
 const Listing = require("../models/listing.js");
-const {validateReview, isLoggedIn} = require("../middleware.js");
+const {validateReview, isLoggedIn, isReviewAuthor} = require("../middleware.js");
 
 
 
@@ -27,7 +27,10 @@ router.post("/",
 }));
 
 //Delete Review Route
-router.delete("/:reviewId" , wrapAsync(async (req,res) => {
+router.delete("/:reviewId" , 
+    isLoggedIn,
+    isReviewAuthor,
+    wrapAsync(async (req,res) => {
     let{id,reviewId} = req.params;
     await Listing.findByIdAndUpdate(id,{$pull:{reviews : reviewId}});
     await Review.findByIdAndDelete(reviewId);
